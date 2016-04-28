af325688cef063ef90cf038e2f059241084c53ea
brianmenard <useyourbrian@gmail.com>
2016-04-28 16:36:04 +0000

    small edits to recipe model

--- dujour/app/models/recipe.rb
+++ dujour/app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
-  #accepts_nested_attributes_for :ingredients
+  accepts_nested_attributes_for :ingredients
 
   def ingredients_attributes=(ingredients_hash)
     # ingredients_hash={
@@ -12,9 +12,8 @@ class Recipe < ActiveRecord::Base
     # }
     ingredients_hash.values.each do |ingredient_attributes|
       ingredient = Ingredient.find_or_create_by(name: ingredient_attributes[:name])
-      self.ingredients << ingredient 
-      #need to figure out how to access amount!!!!
-      #RecipeIngredient.create(recipe_id: self.id, ingredient_id: ingredient.id, amount: "TEST")
+      self.ingredients << ingredient
+      #need to figure out how to access amount
     end
   end
 
