ade3d423f4642058db6387e392b65882aa53700b
brianmenard <useyourbrian@gmail.com>
2016-04-29 13:33:42 +0000

    impleneted omniauth

--- dujour/db/schema.rb
+++ dujour/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427162838) do
+ActiveRecord::Schema.define(version: 20160429124841) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20160427162838) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
