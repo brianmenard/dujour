f08a3196376129fbfcf7b3d7d585db7bc2b8f5ee
Brian <useyourbrian@gmail.com>
2016-07-17 10:48:50 -0400

    added create route for comments

--- dujour/app/controllers/comments_controller.rb
+++ dujour/app/controllers/comments_controller.rb
@@ -1,9 +1,18 @@
 class CommentsController < ApplicationController
 
+  def create
+    @comment = Comment.create(comment_params)
+    render json: @comment
+  end
+
   def show
-    @recipe = Recipe.find(params[:id]
+    @recipe = Recipe.find(params[:id])
     @comments = Recipe.comments
     render json: @comments
   end
+
+def comment_params
+
+end
 
 end
