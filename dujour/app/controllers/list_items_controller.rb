e73b23811f0e39d9eca8c321a00d7ab45652fdce
Brian <useyourbrian@gmail.com>
2016-07-17 15:46:56 -0400

    added create action for listItems, added basic create form to listItems#index

--- dujour/app/controllers/list_items_controller.rb
+++ dujour/app/controllers/list_items_controller.rb
@@ -1,6 +1,18 @@
 class ListItemsController < ApplicationController
 
   def index
+    @list_items = ListItem.all
+    @new_item = ListItem.new
+  end
+
+  def create
+    @item = ListItem.create(item_params)
+    redirect_to list_items_path
+  end
+
+
+  def item_params
+    params.require(:list_item).permit(:content)
   end
 
 end
--- dujour/db/seeds.rb
+++ dujour/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ListItem.create(content: "Item 1")
+ListItem.create(content: "Item 2")
